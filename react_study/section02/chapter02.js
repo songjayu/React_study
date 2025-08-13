1e3d6d79297d2767af367e8b0f9cef9b3fb67910
songjayu <songjayuu@gmail.com>
2025-08-13 22:04:19 +0900

    로컬 git과 github 연결 확인

--- react_study/section02/chapter02.js
+++ react_study/section02/chapter02.js
@@ -24,6 +24,8 @@ function printName(person){
 printName();
 printName({name:"순덕이"})
 
+
+// 실험
 // nn = "zz"
 // const name = nn && "jj";  // 둘 다 Truthy하면 뒤에 걸 인식함
 //     console.log(name || "person의 값이 없음")
