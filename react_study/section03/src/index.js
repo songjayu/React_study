6c5841f3490f3152eb2ca8389c5b983bbce19a5c
songjayu <songjayuu@gmail.com>
2025-08-25 00:10:31 +0900

    250825 리액트 스터디

--- react_study/section03/src/index.js
+++ react_study/section03/src/index.js
@@ -1,10 +1,37 @@
+// CommonJS
+// const moduleData = require("./math");
 
-const moduleData = require("./math");
+// console.log(moduleData.add(1, 2));
+// console.log(moduleData.sub(1, 2));
 
+// console.log(moduleData);
 
-console.log(moduleData);
 
 // 브라우저가 아닌 Node.js로 실행하려면 터미널에 'node (실행할 파일 이름)' 입력
 // 근데 파일경로명이 복잡해질 수 있으니까 package.json에 있는 패키지 스크립트를 이용한다.
 // "start라는 이름을 'node (파일 경로)'에 매칭" --> start만 입력하면 'node (파일 경로)' 실행
 // npm run start 입력
+
+
+// const {add, sub} = require("./math");    // 객체의 구조분해할당 사용 가능
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+
+
+
+
+
+// ESM
+// ESM에서는 모듈의 확장자(.js)까지 명시해야 한다.
+import mul, {add, sub} from "./math.js";  // math 모듈의 default 함수 (그냥 불러와도 어떤 함수를 지칭하는지 아니까, 불러올 때 이름 마음대로 설정 가능 --> mul)
+
+import randomColor from "randomcolor";  // 라이브러리를 쓸 때는 경로를 명시하는 게 아니라 from 뒤에 라이브러리 이름만 명시하면 된다.
+
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(3, 2));
+
--- react_study/section03/src/math.js
+++ react_study/section03/src/math.js
@@ -1,17 +1,33 @@
 // math 모듈
 
-function add(a, b){
+export function add(a, b){ // 함수 앞에 export 키워드를 붙여도 동작(ESM에서)
     return a + b;
 }
 
-function sub(a, b){
+export function sub(a, b){ // 함수 앞에 export 키워드를 붙여도 동작(ESM에서)
     return a - b;
 }
 
+export default function multiply(a, b){ // export 뒤에 default를 붙이면 multiply라는 함수는 default로서 내보내져서 math 모듈을 대표하는 단 하나의 기본값이 된다. 불러올 때 중괄호 없이 혼자 불러와야 함.
+    return a + b;
+}
+
+
+
+// commonJS
 // 모듈이라는 내장 객체에 exports라는 프로퍼티의 값으로 객체를 저장한다.
 // 이러면 CommonJS 모듈 시스템에 의해서 이 두 개의 값이 Math 모듈로부터 내보내지게 된다.
 // 내보내진 값들은 index 같은 다른 모듈에서 내장 함수인 require을 이용해서 이 모듈의 경로를 인수로 전달하면서 불러와서 사용할 수 있다.
-module.exports = {
-    add: add,  // add 라고만 해도 알아먹음
-    sub: sub,  // 얘도 sub
-}
+
+//module.exports = {
+//    add: add,  // add 라고만 해도 알아먹음
+//    sub: sub,  // 얘도 sub
+//}
+
+
+
+
+
+// ESM
+// json 파일에 "type": "module"을 넣으면 패키지가 ES 모듈시스템을 사용하게 된다.
+// export {add, sub};
